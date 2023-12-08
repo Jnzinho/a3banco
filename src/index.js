a855d8e653167391b502a9281cfcbe03013928f1
Jnzinho <joaovitorjn@unochapeco.edu.br>
2023-12-08 20:53:53 -0300

    Adc. express

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
 const { Pool } = require('pg');
 const mongoose = require('mongoose');
-const mongoDBUrl = 'mongodb://localhost:27017/banco4'; // Update with your MongoDB connection string
+const express = require('express');
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+const mongoDBUrl = 'mongodb://localhost:27017/banco4';
 mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Event listener for successful connection
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB at ${mongoDBUrl}`);
 });
 
-// Event listener for connection error
 mongoose.connection.on('error', (err) => {
   console.error(`MongoDB connection error: ${err}`);
 });
 
-// Event listener for disconnected
 mongoose.connection.on('disconnected', () => {
   console.warn('MongoDB disconnected');
 });
 
-// Event listener for application termination
 process.on('SIGINT', () => {
   mongoose.connection.close(() => {
     console.log('MongoDB connection terminated due to application termination');
@@ -32,7 +42,7 @@ const pool = new Pool({
   host: 'localhost',
   database: 'api_deec',
   password: 'admin',
-  port: 5432, // Default PostgreSQL port
+  port: 5432,
 });
 
 pool.query('SELECT NOW()', (err, res) => {
